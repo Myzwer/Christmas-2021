61e986a424d425ebab65cbf708edd7e96580083a
Myzwer <josh@onefortyfivedesign.com>
2021-11-15 16:47:00 -0500

    :art: Finished the Footer design.

--- footer.php
+++ footer.php
@@ -16,41 +16,17 @@
 
 <!--Start Footer-->
 <footer class="footer">
-    <div class="footer-left">
-
-        <!--Company Logo-->
-        <img src="https://images.squarespace-cdn.com/content/v1/575a6067b654f9b902f452f4/1552683653140-0UUVQSSUEWVC73AWAEQG/300Logo.png">
-        <div class="left-inner">
-            <!--Company Info-->
-            <h3>Fancy Company</h3>
-            <p class="phone-number">+1 234-567-8901</p>
-        </div>
+    <div class="footer-icons">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="#"><img src="http://christmas-2021.local/wp-content/uploads/2021/11/Footer-Icon.png" alt=""></a>
     </div>
 
-    <div class="footer-right">
-        <div class="right-inner">
-            <!--Wordpress Generated Link Lists-->
-            <?php wp_nav_menu( array( 'theme_location' => 'footer-column-1' ) ); ?>
-            <?php wp_nav_menu( array( 'theme_location' => 'footer-column-2' ) ); ?>
-        </div>
-
-    </div>
-    <div class="footer-bottom">
-        <div class="bot-inner">
-
-            <!--Copyright Info-->
-            <p>Some Text</p>
-            <p>© <?php echo date("Y"); ?> Website Company Name</p>
-
-            <!--Socials, icons from: https://fontawesome.com/ -->
-            <div class="footer-icons">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-instagram"></i></a>
-            <a href="#"><i class="fa fa-linkedin"></i></a>
-            <a href="#"><i class="fa fa-youtube"></i></a>
-        </div>
-    </div>
+    <!--Wordpress Generated Link Lists-->
+    <?php wp_nav_menu(array('theme_location' => 'footer-column-1')); ?>
+    <!--Copyright Info-->
+    <p class = "copyright">© <?php echo date("Y"); ?> Foothills Church. You Belong Here.</p>
 </footer>
 <!--End Footer-->
 
