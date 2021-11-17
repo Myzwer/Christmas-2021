a6a6d7066d76854e0f59d1fe71b82ec8f82d37c0
Myzwer <josh@onefortyfivedesign.com>
2021-11-17 15:29:34 -0500

    :rocket: Got the invite cards section set up. Seems to work on both desktop and mobile.

--- frontpage.php
+++ frontpage.php
@@ -47,8 +47,7 @@ get_header(); ?>
             </div>
         </div>
     </div>
-<!--End Modal-->
-
+    <!--End Modal-->
 
 
     <div id="particles-js"></div>
@@ -56,9 +55,11 @@ get_header(); ?>
     url('http://christmas-2021.local/wp-content/uploads/2021/11/AFC-Background-Banner.jpg') bottom center;
  height: 60vh;">
         <div class="text-center relative pt-24 lg:p-36 z-5">
-            <img class = "mx-auto w-11/12 md:w-8/12 lg:w-5/12" src="http://christmas-2021.local/wp-content/uploads/2021/11/A-Foothills-Christmas-Icon-Alt.png" alt="">
+            <img class="mx-auto w-11/12 md:w-8/12 lg:w-5/12"
+                 src="http://christmas-2021.local/wp-content/uploads/2021/11/A-Foothills-Christmas-Icon-Alt.png" alt="">
 
-            <h1 class = "uppercase text-red text-2xl md:text-3xl py-5 px-10 z-5">A place to <span class = "text-green">belong</span> this christmas.</h1>
+            <h1 class="uppercase text-red text-2xl md:text-3xl py-5 px-10 z-5">A place to <span class="text-green">belong</span>
+                this christmas.</h1>
 
             <a download href="http://christmas-2021.local/wp-content/uploads/2021/11/afoothillschristmas.ics">
                 <button class="mx-auto lg:mx-0  bg-yellow text-black rounded-full my-1 md:my-1 py-4 px-5 md:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out z-5">
@@ -66,130 +67,49 @@ get_header(); ?>
                 </button>
             </a>
         </div>
-        <img class = "hidden lg:block absolute left-36 bottom-3" src="http://christmas-2021.local/wp-content/uploads/2021/11/Guy-with-Boxes.png" alt="">
-        <img class = "hidden lg:block absolute right-36 bottom-3" src="http://christmas-2021.local/wp-content/uploads/2021/11/GIrl-with-Bags.png" alt="">
+        <img class="hidden lg:block absolute left-36 bottom-3"
+             src="http://christmas-2021.local/wp-content/uploads/2021/11/Guy-with-Boxes.png" alt="">
+        <img class="hidden lg:block absolute right-36 bottom-3"
+             src="http://christmas-2021.local/wp-content/uploads/2021/11/GIrl-with-Bags.png" alt="">
     </div>
 
-    <div class="bg-white pb-10">
-        <div class=" lg:max-w-4xl lg:text-center lg:mx-auto pt-10">
-            <div class="grid">
-                <div class=" grid-cols-12">
-                    <div class="col-span-12">
-                        <div class="text-center md:text-left mb-1">
-                            <h1>Color Pod</h1>
-                            <p>All fields optional</p>
-                        </div>
-                        <hr>
+    <div class="bg-no-repeat bg-scroll bg-cover relative pb-10" style="background:
+    url('http://christmas-2021.local/wp-content/uploads/2021/11/Home-Noise-Background.jpg') bottom center;">
+        <div class="grid grid-cols-12 gap-4 lg:gap-4 lg:w-7/12 mx-4 lg:mx-auto pt-10 text-white">
+            <div class="col-span-12 md:col-span-6 bg-red rounded-lg shadow-xl p-4">
+                <div class="border-2 rounded-md">
+                    <div class="text-left uppercase p-2">
+                        <h2 class=" text-xl text-center pb-5">Join us in Maryville</h2>
+                        <p class="font-light"><span class="font-black">When:</span> December 19th</p>
+                        <p><span class="font-black">Time:</span> 9am & 11am</p>
+                        <p><span class="font-black">Where:</span> 1551 W. lamar Alexander Pkwy. <br />maryville tn 37801</p>
                     </div>
-                </div>
-            </div>
-
-            <div class="grid grid-cols-12 gap-4 mt-6">
-                <div class="col-span-12 lg:col-span-4 card-gradient-1 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class="font-bold text-2xl">Title</h2>
-                        <h4 class="font-semibold">Subtitle</h4>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam
-                            commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore
-                            magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
+                    <div class="text-center mb-5">
+                        <button class="mx-auto lg:mx-0 bg-yellow text-black rounded-full my-1 md:my-1 py-3 px-4 md:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out z-5">
+                            Mark Your Calendar <i class="far fa-calendar-alt"></i>
                         </button>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-4 card-gradient-2 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class="font-bold text-2xl">Title</h2>
-                        <h4 class="font-semibold">Subtitle</h4>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam
-                            commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore
-                            magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-4 card-gradient-3 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class="font-bold text-2xl">Title</h2>
-                        <h4 class="font-semibold">Subtitle</h4>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam
-                            commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore
-                            magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
+                        <button class="mx-auto lg:mx-0  border-white border-2 text-white font-bold rounded-full my-1 md:my-1 py-2 px-7 md:px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                            More Information
                         </button>
                     </div>
                 </div>
             </div>
 
-            <div class="grid grid-cols-12 mt-20">
-                <div class="col-span-12">
-                    <div class="text-center md:text-left mb-1">
-                        <h1>PDF Download (four-up)</h1>
-                        <p>This configuration always shows four PDF's on one row.</p>
+            <div class="col-span-12 md:col-span-6 bg-green rounded-lg shadow-xl p-4">
+                <div class="border-2 rounded-md">
+                    <div class="text-left uppercase p-2">
+                        <h2 class=" text-xl text-center pb-5">Join us in Bearden</h2>
+                        <p class="font-light"><span class="font-black">When:</span> December 19th</p>
+                        <p><span class="font-black">Time:</span> 11am</p>
+                        <p><span class="font-black">Where:</span> 1000 Francis Rd. <br /> Knoxville, TN 37909</p>
                     </div>
-                    <hr>
-                </div>
-            </div>
-
-
-            <div class="grid grid-cols-12 mt-5 gap-6">
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class="rounded-full"
-                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-                                 alt="">
-                        </div>
-                        <h2 class="font-bold text-2xl">PDF Title</h2>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis
-                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio
-                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class="rounded-full"
-                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-                                 alt="">
-                        </div>
-                        <h2 class="font-bold text-2xl">PDF Title</h2>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis
-                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio
-                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class="rounded-full"
-                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-                                 alt="">
-                        </div>
-                        <h2 class="font-bold text-2xl">PDF Title</h2>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis
-                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio
-                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class="rounded-full"
-                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-                                 alt="">
-                        </div>
-                        <h2 class="font-bold text-2xl">PDF Title</h2>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis
-                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio
-                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
+                    <div class="text-center mb-5">
+                        <button class="mx-auto lg:mx-0 bg-yellow text-black rounded-full my-1 md:my-1 py-3 px-4 md:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out z-5">
+                            Mark Your Calendar <i class="far fa-calendar-alt"></i>
+                        </button>
+                        <button class="mx-auto lg:mx-0  border-white border-2 text-white font-bold rounded-full my-1 md:my-1 py-2 px-7 md:px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                            More Information
+                        </button>
                     </div>
                 </div>
             </div>
