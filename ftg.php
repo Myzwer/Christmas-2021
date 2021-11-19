44d690ab97f1d698aacd60f2e0ff2349798f4145
Myzwer <josh@onefortyfivedesign.com>
2021-11-19 03:02:33 -0500

    :rocket: Header is done for now. Might need to add a dude to the mobile version but the ftg header is finished as of now.

--- ftg.php
+++ ftg.php
@@ -13,7 +13,55 @@
 
 get_header(); ?>
 
-    <!--Get it done-->
+
+    <!-- The Modal -->
+    <div id="modal-container">
+        <div class="modal-background">
+            <div class="modal">
+                <div class="modal-content">
+                    <div class="modal-top bg-no-repeat bg-scroll bg-cover relative"
+                         style="background: url('http://christmas-2021.local/wp-content/uploads/2021/11/Modal-header.jpg') center center;">
+
+                        <div class="modal-icon">
+                            <img src="http://christmas-2021.local/wp-content/uploads/2021/11/FC-logo-1.png" alt="">
+                        </div>
+                    </div>
+                    <div class="modal-inner">
+                        <p class="pb-5">You can add the event to you calendar now or get more information. We can't wait
+                            to see
+                            you!</p>
+                        <a download
+                           href="http://christmas-2021.local/wp-content/uploads/2021/11/afoothillschristmas.ics">
+                            <button class="mx-auto lg:mx-0 w-full bg-yellow text-black font-bold rounded-full my-1 md:my-1 py-4 px-5 md:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                <i class="far fa-calendar-alt"></i> Add To Calendar
+                            </button>
+                        </a>
+
+                        <a href="#">
+                            <button class="mx-auto lg:mx-0 w-full border-black border-2 text-black font-bold rounded-full my-1 md:my-1 py-3 px-6 md:px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                More Information
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End Modal-->
+
+    <div id="particles-js"></div>
+
+    <div class="bg-no-repeat bg-scroll bg-cover relative" style="background:
+    url('http://christmas-2021.local/wp-content/uploads/2021/11/AFC-Background-Banner.jpg') bottom center; height: 90vh;">
+        <div class=" invisible md:visible bg-no-repeat bg-scroll bg-cover relative" style="background:
+    url('http://christmas-2021.local/wp-content/uploads/2021/11/All-People.png') bottom center; height: 90vh;">
+            <div class="visible text-left relative pt-10 px-3 z-5">
+                <h1 class="uppercase text-red text-6xl lg:text-8xl font-black leading-ish">You Belong here</h1>
+                <p class="capitalize text-red font-bold text-lg lg:text-2xl  pr-10">We’d love to see you and your family for A Foothills Christmas!</p>
+            </div>
+        </div>
+    </div>
+
 
 <?php
 get_footer();
