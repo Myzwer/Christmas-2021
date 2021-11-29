63fe04653e990053e5b1d8de694ed684b571d01d
Myzwer <josh@onefortyfivedesign.com>
2021-11-29 15:41:58 -0500

    :bug:  Trying my best to configure performance settings on this site. Removed gutenberg and deferred font awesome.

--- functions.php
+++ functions.php
@@ -98,6 +98,19 @@ add_theme_support( 'post-thumbnails' );
 
 
 //*****************************************************
+//****************** DIE GUTENBERG ********************
+//*****************************************************
+function smartwp_remove_wp_block_library_css()
+{
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('wc-block-style'); // Remove WooCommerce block CSS
+}
+
+add_action('wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100);
+
+
+//*****************************************************
 //******************* PAGINATION **********************
 //*****************************************************
 
