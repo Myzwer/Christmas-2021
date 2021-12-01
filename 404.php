d5859646522ecf7eda6134d2d96c338e5d98a827
Myzwer <josh@onefortyfivedesign.com>
2021-12-01 16:59:44 -0500

    :rocket: Updated the 404 page to actually be Christmas..sy. It isn't the the one that ships with prelaunch anymore.

--- 404.php
+++ 404.php
@@ -14,28 +14,30 @@
 get_header(); ?>
 
     <div class="grid grid-cols-12">
-        <div class="col-span-12 md:col-span-6 text-center">
+        <div class="col-span-12 md:col-span-6 text-left md:text-center m-5">
             <div class="md:text-left md:mx-10">
-                <div class="w-20 mt-11">
-                    <img src="https://images.squarespace-cdn.com/content/v1/575a6067b654f9b902f452f4/1552683653140-0UUVQSSUEWVC73AWAEQG/300Logo.png"
-                         alt="Company Logo">
+                <h1 class="uppercase text-3xl mb-3 md:mt-16">404 Error</h1>
+                <h2 class="font-bold text-lg">Looks like the grinch stole this page!</h2>
+                <p class="text-sm">We'll try to get that fixed. For now you can:</p>
+                <div class="md:mt-5">
+                    <?php if (have_rows('back_button', 521)): ?>
+                        <?php while (have_rows('back_button', 521)): the_row(); ?>
+                            <a href="<?php the_sub_field('button_link', 521); ?>">
+                                <button class="mx-auto lg:mx-0 border-black border-2 text-black font-bold rounded-full my-1 md:my-1 py-2 px-7 md:px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                    <?php the_sub_field('button_text', 521); ?>
+                                </button>
+                            </a>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                 </div>
-
-                <p class="md:mt-10">404 Error</p>
-                <h1 class="uppercase text-2xl md:text-3xl md:mb-10">Page Not Found</h1>
-                <p>The rocket took off without this page ¯\_(ツ)_/¯</p>
-                <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                    Back to Homepage
-                </button>
             </div>
         </div>
 
         <div class="col-span-12 md:col-span-6">
             <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
-				  rgba(0, 0, 0, 0.45),
-				  rgba(0, 0, 0, 0.45)
-				), url('https://images.unsplash.com/photo-1501612780327-45045538702b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80') center center;
-				 height: 80vh;">
+                    rgba(0, 0, 0, 0.45),
+                    rgba(0, 0, 0, 0.45)
+                    ), url('<?php the_field('background_image', 521); ?>') no-repeat bottom center scroll; background-size: cover; height: 80vh;">
             </div>
         </div>
     </div>
